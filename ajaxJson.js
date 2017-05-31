11201db065d86f5248f55a55078e5f612bf01985
Nick Tanaka <nicktanaka@Nicks-MacBook-Air-2.local>
2017-05-31 14:58:16 -0700

    added error check

--- ajaxJson.js
+++ ajaxJson.js
@@ -1,39 +1,54 @@
 function sunriseSunset() {
-var latitude = document.getElementById("latitude").value;
-var longitude = document.getElementById("longitude").value;
-var date = document.getElementById("date").value;
-// Now make a HTTP request
+    errorCheck();
+    var latitude = document.getElementById("latitude").value;
+    var longitude = document.getElementById("longitude").value;
+    var date = document.getElementById("date").value;
+    // Now make a HTTP request
     var httpRequest = new XMLHttpRequest();
     httpRequest.onreadystatechange = function () {
         if (this.readyState === 4) {
             // We got a response from the server!
-            if(this.status === 200) {
+            if (this.status === 200) {
                 // The request was successful!
                 displaySun(this.responseText);
-            } else if (this.status === 404){
-
-                displaySun('{ "make" : "none" }');
-            } else {
+            }
+            else if (this.status === 404) {
+                displaySun('{ "sunrise" : "none" }');
+            }
+            else {
                 console.log("We have a problem...server responded with code: " + this.status);
             }
-        } else {
+        }
+        else {
             // Waiting for a response...
         }
     };
-
- var url = "https://api.sunrise-sunset.org/json?lat=" + latitude + "&" + "lng=" + longitude + "&" + "date=" + date;
+    var url = "https://api.sunrise-sunset.org/json?lat=" + latitude + "&" + "lng=" + longitude + "&" + "date=" + date;
     httpRequest.open("GET", url, true);
     httpRequest.send();
 }
 
-
-function displaySun(data){
+function displaySun(data) {
     var sun = JSON.parse(data);
-    if(sun.results.sunrise === "none" || sun.results.sunset === "none") {
+    document.getElementById("sun").className = "alert alert-success";
+    document.getElementById("sun").innerHTML = "Sunrise: " + sun.results.sunrise + " Sunset: " + sun.results.sunset;
+    errorCheck();
+}
+
+function errorCheck() {
+    var latitude = document.getElementById("latitude").value;
+    var longitude = document.getElementById("longitude").value;
+    var date = document.getElementById("date").value;
+    if (latitude < -90 || latitude > 90) {
         document.getElementById("sun").className = "alert alert-warning";
-        document.getElementById("sun").innerHTML = "No place matches that zip code."
-    } else {
-        document.getElementById("sun").className = "alert alert-success";
-        document.getElementById("sun").innerHTML = "Sunrise: " + sun.results.sunrise + " Sunset: " + sun.results.sunset;
-}
-}
+        document.getElementById("sun").innerHTML = "Invalid inputs. Please enter a valid latitude and longitude and be sure that the date is entered in the correct format.";
+    }
+    else if (longitude < -180 || latitude > 180) {
+        document.getElementById("sun").className = "alert alert-warning";
+        document.getElementById("sun").innerHTML = "Invalid inputs. Please enter a valid latitude and longitude and be sure that the date is entered in the correct format.";
+    }
+    else if (date.indexOf("-") != 4 || date.indexOf("-") != 7) {
+        document.getElementById("sun").className = "alert alert-warning";
+        document.getElementById("sun").innerHTML = "Invalid inputs. Please enter a valid latitude and longitude and be sure that the date is entered in the correct format.";
+    else {}
+    }
