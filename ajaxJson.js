a55059e072645b3844fdb7160e74d8977bf02b07
Nick Tanaka <nicktanaka@Nicks-MacBook-Air-2.local>
2017-05-31 13:33:42 -0700

    finished

--- ajaxJson.js
+++ ajaxJson.js
@@ -1,23 +1,18 @@
-/**
- * Uses AJAX to query an internet data source for zip codes
- * @param {string} zipId The element id that has the zip code
- */
-function findMake(make) {
-    // First get the zip code from the HTML textbox
-    var make = document.getElementById(make).value;
-    var model = document.getElementById(model).value;
-    var year = document.getElementById(year).value;
-    // Now make a HTTP request
+function sunriseSunset() {
+var latitude = document.getElementById("latitude").value;
+var longitude = document.getElementById("longitude").value;
+var date = document.getElementById("date").value;
+// Now make a HTTP request
     var httpRequest = new XMLHttpRequest();
     httpRequest.onreadystatechange = function () {
         if (this.readyState === 4) {
             // We got a response from the server!
             if(this.status === 200) {
                 // The request was successful!
-                displayCar(this.responseText);
+                displaySun(this.responseText);
             } else if (this.status === 404){
-                // No postal code found
-                displayCar('{ "make" : "none" }');
+
+                displaySun('{ "make" : "none" }');
             } else {
                 console.log("We have a problem...server responded with code: " + this.status);
             }
@@ -25,27 +20,19 @@ function findMake(make) {
             // Waiting for a response...
         }
     };
-    // Notice how the URL is appended with the zip code
-    var url = "https://api.edmunds.com/api/vehicle/v2/" + make + "/" + model + "/years?fmt=json&year=" + year +  "&api_key=um6rcmcsyjx3yyyxwfhp6grb";
+ var url = "https://api.sunrise-sunset.org/json?lat=" + latitude + "&" + "lng=" + longitude + "&" + "date=" + date;
     httpRequest.open("GET", url, true);
     httpRequest.send();
 }
 
-/**
- * Displays the zip code place given the JSON data
- * @param {string} data JSON data representing place for given zip code
- */
-function displayCar(data){
-    var car = JSON.parse(data);
-    if(place.country === "none") {
-        document.getElementById("place").className = "alert alert-warning";
-        document.getElementById("place").innerHTML = "No place matches that zip code."
+
+function displaySun(data){
+    var sun = JSON.parse(data);
+    if(sun.results.sunrise === "none" || sun.results.sunset === "none") {
+        document.getElementById("sun").className = "alert alert-warning";
+        document.getElementById("sun").innerHTML = "No place matches that zip code."
     } else {
-        document.getElementById("place").className = "alert alert-success";
-        document.getElementById("place").innerHTML = place.places[0]["place name"] +
-        ", " +
-        place.places[0].state +
-        ", " +
-        place.country;
-    }
+        document.getElementById("sun").className = "alert alert-success";
+        document.getElementById("sun").innerHTML = "Sunrise: " + sun.results.sunrise + " Sunset: " + sun.results.sunset;
+}
 }
