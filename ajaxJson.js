bb1df2e100aa6493ac404a9a44581af30ba9fd88
Nick Tanaka <nicktanaka@Nicks-MacBook-Air-2.local>
2017-06-05 11:54:50 -0700

    DONE

--- ajaxJson.js
+++ ajaxJson.js
@@ -1,5 +1,4 @@
 function sunriseSunset() {
-    errorCheck();
     var latitude = document.getElementById("latitude").value;
     var longitude = document.getElementById("longitude").value;
     var date = document.getElementById("date").value;
@@ -24,41 +23,41 @@ function sunriseSunset() {
         }
     };
     var url = "https://api.sunrise-sunset.org/json?lat=" + latitude + "&" + "lng=" + longitude + "&" + "date=" + date;
-
     httpRequest.open("GET", url, true);
     httpRequest.send();
 }
 
-
 function displaySun(data) {
     var sun = JSON.parse(data);
-    if (sun.results.sunrise === "none" || sun.results.sunrise === "none") {
-        document.getElementById("sun").className = "alert alert-warning";
-        document.getElementById("sun").innerHTML = "No data could be found."
-    }
-    else {
+    var sunriseString = sun.results.sunrise;
+    var sunsetString = sun.results.sunset;
+    if (sunriseString.length >= 10 && sunsetString.length >= 10) {
         document.getElementById("sun").className = "alert alert-success";
-        document.getElementById("sun").innerHTML = "Sunrise: " + sun.results.sunrise + "     " + "Sunset: " + sun.results.sunset;
+        document.getElementById("sun").innerHTML = "Sunrise: " + sunriseString + "     " + "Sunset: " + sunsetString;
     }
-    errorCheck();
+    else if (sun.results.sunrise == "undefined" || sun.results.sunset == "undefined") {
+        document.getElementById("sun").className = "alert alert-warning";
+        document.getElementById("sun").innerHTML = "No data could be found. Please enter numerical values."
+    }
 }
 
 function errorCheck() {
     var latitude = document.getElementById("latitude").value;
     var longitude = document.getElementById("longitude").value;
     var date = document.getElementById("date").value;
-    if (latitude < -90 || latitude > 90) {
+    if (!(latitude > -90 && latitude < 90)) {
         document.getElementById("sun").className = "alert alert-warning";
-        document.getElementById("sun").innerHTML = "Invalid inputs. Please enter a valid latitude and longitude and be sure that the date is entered in the correct format.";
+        document.getElementById("sun").innerHTML = "Latitude input error. Please enter a latitude value in between -90 and 90.";
     }
-    else if (longitude < -180 || latitude > 180) {
+    else if (!(longitude > -180 && longitude < 180)) {
         document.getElementById("sun").className = "alert alert-warning";
-        document.getElementById("sun").innerHTML = "Invalid inputs. Please enter a valid latitude and longitude and be sure that the date is entered in the correct format.";
+        document.getElementById("sun").innerHTML = "Longitude input error. Please enter a longitude value in between -180 and 180.";
     }
     else if (date.indexOf("-") != 4 && date.indexOf("-") != 7) {
         document.getElementById("sun").className = "alert alert-warning";
-        document.getElementById("sun").innerHTML = "Invalid inputs. Please enter a valid latitude and longitude and be sure that the date is entered in the correct format.";
-
+        document.getElementById("sun").innerHTML = "Date input error. Please enter a date in the format YYYY-MM-DD.";
     }
-    else {}
+    else {
+        sunriseSunset();
+    }
 }
